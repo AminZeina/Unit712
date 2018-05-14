45741e6909fc2d568a3e37d6317b531c8e0d040d
Amin Zeina <amin.zeina@ocsbstudent.ca>
2018-05-14 13:30:09 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created By: Amin Zeina 
+-- Created On: May 2018
+-----------------------------------------------------------------------------------------
+
+local composer = require( 'composer' )
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene( 'scene1' )
